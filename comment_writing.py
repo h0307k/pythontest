dfe87eb9e7ad59db82296d4a2a087be802f930a9
工藤浩 <hprek20160101@gmail.com>
2017-06-01 23:55:01 +0900

    コメント(#)

---
+++ comment_writing.py
@@ -0,0 +1,8 @@
+print("Hello")  # Helloと表示します
+total = "123" \
+    + "456" # ここには記述できるが、上記には記述できない
+print(total)
+months = ['Jan','Feb','Mar','Apr',  # ここにはコメントを記述する
+          'May','Jun','Jul','Aug',  # ここにはコメントを記述する
+          'Sep','Oct','Nov','Dec']
+print(months)
