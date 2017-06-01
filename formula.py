ea0d5c6a566186e68758fcc54c01fcf472dd1409
工藤浩 <hprek20160101@gmail.com>
2017-06-01 23:42:37 +0900

    文、式の扱い方

---
+++ formula.py
@@ -0,0 +1,19 @@
+"""
+文、
+"""
+
+# Perl や PHP の様にセミコロン(;)で区切って表示する
+a = 5; b = 3; c = a + b
+print(c)
+
+# 文と数行に分けて記述する
+total = "123"\
+    + "456"\
+    + "789"
+print(total)
+
+# カンマの後ろはバックスラッシュを省略可能
+months = ['Jan','Feb','Mar','Apr',
+          'May','Jun','Jul','Aug',
+          'Sep','Oct','Nov','Dec']
+print(months)
